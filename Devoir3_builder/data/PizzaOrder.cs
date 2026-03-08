d639afdd628b7282152311d3f7a12d1ed9512fe1
Lililatortue <will.descoteaux@gmail.com>
2026-03-08 14:21:47 -0400

    uniformisation des class

---
+++ Devoir3_builder/data/PizzaOrder.cs
@@ -0,0 +1,16 @@
+﻿using Devoir3_builder.Observer;
+using Devoir3_builder.State;
+using Devoir6.decorator;
+
+namespace Devoir3_builder.data
+{
+    public class PizzaOrder
+    {
+        public IPizzaDecorator pizza;
+        public IOrderState state;
+        private List<INotify> observers = [];
+
+
+
+    }
+}
